e6ec1e50f0afa3685d45f78e18e5cd1e88a01cbc
pratik hirpara <pkhirparapk@outlook.com>
2021-02-24 11:51:00 +0100

    Logout and Back to Dashboard in Admin Panel

--- admin/admindash.php
+++ admin/admindash.php
@@ -19,7 +19,10 @@
 
 ?>
 	<div class="admintitle" align="center">
-		<h4><a href="logout.php" style="float:right; margin-right: 30px; color: #A52A2A; font-size:20px;">Logout</a></h4>
+		<h4>
+			
+			<a href="logout.php" style="float:right; margin-right: 30px; color: #A52A2A; font-size:20px;">Logout</a>
+		</h4>
 		<h1>Welcome to Admin Dashboard</h1>
 
 	</div>
--- admin/titlehead.php
+++ admin/titlehead.php
@@ -1,5 +1,9 @@
 <div class="admintitle" align="center">
-		<h4><a href="logout.php" style="float:right; margin-right: 30px; color: #A52A2A; font-size:20px;">Logout</a></h4>
+		<h4>
+			<a href="admindash.php" style="float:left; margin-right: 30px; color: #A52A2A; font-size:20px;">Back to Dashboard</a>
+
+			<a href="logout.php" style="float:right; margin-right: 30px; color: #A52A2A; font-size:20px;">Logout</a>
+		</h4>
 		<h1>Welcome to Admin Dashboard</h1>
 
 	</div>
